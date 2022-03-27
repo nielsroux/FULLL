b02a98fb182fbed779f397b5f7a0159ce4e3d6cf
nielsroux <nielsroux@gmail.com>
2022-03-27 14:44:27 +0200

    [Algo] Custom Number Type Increment Added

---
+++ algo-custom-number-type-increment/algo.js
@@ -0,0 +1,29 @@
+/*
+ * JavaScript implementation for Adding one
+ * to number represented by digits in Array
+ */
+
+//Add 1 to the input Array<number> and return the incremented Array<number>
+const increment = (number) => {
+    // Start with last index
+    let index = number.length - 1;
+
+    // while the index is valid and the value at [index] ==
+    // 9 set it as 0
+    while (number[index] == 9) number[index--] = 0;
+
+    // if index < 0 (if all digits were 9)
+    if (index < 0)
+        // insert an one at the beginning of the vector
+        number.unshift(1);
+    // else increment the value at [index]
+    else number[index]++;
+
+    return number;
+};
+
+let number = [9, 9];
+
+for (let index = 0; index < 150; index++) {
+    console.log(increment(number));
+}
